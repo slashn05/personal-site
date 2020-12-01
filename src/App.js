42cd5d3e0a3a7b90fa443ce0e3148c1c1b0b978d
Sandeep Edara <Sandeep.Edara@walmartlabs.com>
2020-12-01 21:56:35 +0530

    Content updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="main-content">
+        <div>Sandeep Edara</div>
+        <div className="sub-content">
+          <div className="sub-heading">Software Engineer, Walmart</div>
+          <div className="sub-heading">Bengaluru</div>
+        </div>
+      </div>
     </div>
   );
 }
